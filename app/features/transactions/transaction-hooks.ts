cc93350cb056a650a2d4fb26ae6808ab16ba196a
gudnuf <gudnuf21@proton.me>
2025-09-04 10:30:31 -0700

    add onAcknowlededStatusChange callback to realtime

--- app/features/transactions/transaction-hooks.ts
+++ app/features/transactions/transaction-hooks.ts
@@ -41,8 +41,6 @@ export class TransactionsCache {
    * @param transaction - The updated transaction.
    */
   update(transaction: Transaction) {
-    this.handleAcknowledgmentStatusChange(transaction);
-
     this.queryClient.setQueryData<Transaction>(
       [transactionQueryKey, transaction.id],
       transaction,
@@ -64,20 +62,14 @@ export class TransactionsCache {
     );
   }
 
-  private handleAcknowledgmentStatusChange(newTransaction: Transaction) {
-    const oldTransaction = this.queryClient.getQueryData<Transaction>([
-      transactionQueryKey,
-      newTransaction.id,
-    ]);
+  incrementUnacknowledgedCount() {
+    const currentCount = this.getUnacknowledgedCount();
+    this.setUnacknowledgedCount(currentCount + 1);
+  }
 
-    const oldStatus = oldTransaction?.acknowledgmentStatus;
-    const newStatus = newTransaction.acknowledgmentStatus;
-
-    if (oldStatus !== 'pending' && newStatus === 'pending') {
-      this.incrementUnacknowledgedCount();
-    } else if (oldStatus === 'pending' && newStatus === 'acknowledged') {
-      this.decrementUnacknowledgedCount();
-    }
+  decrementUnacknowledgedCount() {
+    const currentCount = this.getUnacknowledgedCount();
+    this.setUnacknowledgedCount(currentCount - 1);
   }
 
   private getUnacknowledgedCount(): number {
@@ -93,16 +85,6 @@ export class TransactionsCache {
       [unacknowledgedTransactionsCountQueryKey],
       Math.max(0, count), // Ensure count never goes negative
     );
-  }
-
-  private incrementUnacknowledgedCount() {
-    const currentCount = this.getUnacknowledgedCount();
-    this.setUnacknowledgedCount(currentCount + 1);
-  }
-
-  private decrementUnacknowledgedCount() {
-    const currentCount = this.getUnacknowledgedCount();
-    this.setUnacknowledgedCount(currentCount - 1);
   }
 }
 
@@ -292,13 +274,19 @@ export function useReverseTransaction({
 function useOnTransactionChange({
   onCreated,
   onUpdated,
+  onAckStatusChange,
 }: {
   onCreated: (transaction: Transaction) => void;
   onUpdated: (transaction: Transaction) => void;
+  onAckStatusChange: (
+    newStatus: Transaction['acknowledgmentStatus'],
+    prevStatus: Transaction['acknowledgmentStatus'],
+  ) => void;
 }) {
   const transactionRepository = useTransactionRepository();
   const onCreatedRef = useLatest(onCreated);
   const onUpdatedRef = useLatest(onUpdated);
+  const onAckStatusChangeRef = useLatest(onAckStatusChange);
   const queryClient = useQueryClient();
 
   return useSupabaseRealtimeSubscription({
@@ -323,6 +311,16 @@ function useOnTransactionChange({
               await transactionRepository.toTransaction(payload.new);
 
             onUpdatedRef.current(updatedTransaction);
+
+            if (
+              payload.new.acknowledgment_status !==
+              payload.old.acknowledgment_status
+            ) {
+              onAckStatusChangeRef.current(
+                updatedTransaction.acknowledgmentStatus,
+                payload.old.acknowledgment_status ?? null,
+              );
+            }
           }
         },
       ),
@@ -349,6 +347,13 @@ export function useTrackTransactions() {
     },
     onUpdated: (transaction) => {
       transactionsCache.update(transaction);
+    },
+    onAckStatusChange: (newStatus, prevStatus) => {
+      if (prevStatus === null && newStatus === 'pending') {
+        transactionsCache.incrementUnacknowledgedCount();
+      } else if (prevStatus === 'pending' && newStatus === 'acknowledged') {
+        transactionsCache.decrementUnacknowledgedCount();
+      }
     },
   });
 }
