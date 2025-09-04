39559493b3fcc5807a715c5938ad5f28bf2b8112
gudnuf <gudnuf21@proton.me>
2025-09-04 10:19:37 -0700

    move ack status store to its own file

--- app/features/transactions/transaction-list.tsx
+++ app/features/transactions/transaction-list.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react';
 import { Card } from '~/components/ui/card';
 import { ScrollArea } from '~/components/ui/scroll-area';
+import { useTransactionAckStatusStore } from '~/features/transactions/transaction-ack-status-store';
 import { useIsVisible } from '~/hooks/use-is-visible';
 import { LinkWithViewTransition } from '~/lib/transitions';
 import { useLatest } from '~/lib/use-latest';
--- app/routes/_protected.transactions.tsx
+++ app/routes/_protected.transactions.tsx
@@ -1,49 +1,6 @@
 import { useState } from 'react';
-import { Outlet, useOutletContext } from 'react-router';
-import { create, useStore } from 'zustand';
-import type { Transaction } from '~/features/transactions/transaction';
-
-type TransactionAckStatusState = {
-  statuses: Map<string, Transaction['acknowledgmentStatus']>;
-  setIfMissing: (transaction: Transaction) => void;
-  setAckStatus: (transaction: Transaction) => void;
-};
-
-const createTransactionAckStatusStore = () => {
-  return create<TransactionAckStatusState>((set, get) => ({
-    statuses: new Map(),
-    setIfMissing: (transaction: Transaction) => {
-      const { statuses, setAckStatus } = get();
-      if (statuses.has(transaction.id)) {
-        return;
-      }
-      setAckStatus(transaction);
-    },
-    setAckStatus: (transaction: Transaction) => {
-      const { statuses } = get();
-      set({
-        statuses: new Map(statuses).set(
-          transaction.id,
-          transaction.acknowledgmentStatus,
-        ),
-      });
-    },
-  }));
-};
-
-type TransactionAckStatusStore = ReturnType<
-  typeof createTransactionAckStatusStore
->;
-
-export function useTransactionAckStatusStore() {
-  const store = useOutletContext<TransactionAckStatusStore>();
-  if (!store) {
-    throw new Error(
-      'TransactionAckStatusStore not found in router outlet context',
-    );
-  }
-  return useStore(store);
-}
+import { Outlet } from 'react-router';
+import { createTransactionAckStatusStore } from '~/features/transactions/transaction-ack-status-store';
 
 export default function TransactionsLayout() {
   const [store] = useState(() => createTransactionAckStatusStore());
