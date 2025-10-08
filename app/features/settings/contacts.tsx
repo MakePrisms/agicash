b56f5a79aad8e255803d5fdaaa68242415fdea0a
gudnuf <gudnuf21@proton.me>
2025-10-08 07:42:15 -0700

    add suspense boundary to contacts

--- app/features/settings/contacts.tsx
+++ app/features/settings/contacts.tsx
@@ -3,16 +3,9 @@ import { PageContent } from '~/components/page';
 import { AddContactDrawer, ContactsList } from '~/features/contacts';
 import { SettingsViewHeader } from '~/features/settings/ui/settings-view-header';
 import { SearchBar } from '../../components/search-bar';
-import { useContacts } from '../contacts/contact-hooks';
 
 export default function Contacts() {
   const [searchQuery, setSearchQuery] = useState('');
-
-  const contacts = useContacts((contacts) =>
-    contacts.filter((contact) =>
-      contact.username.toLowerCase().includes(searchQuery.toLowerCase()),
-    ),
-  );
 
   return (
     <>
@@ -29,7 +22,7 @@ export default function Contacts() {
       <PageContent className="overflow-hidden">
         <SearchBar onSearch={setSearchQuery} placeholder="Search contacts..." />
 
-        <ContactsList contacts={contacts} />
+        <ContactsList searchQuery={searchQuery} />
       </PageContent>
     </>
   );
