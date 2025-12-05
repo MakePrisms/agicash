a10e5c5cd11db6ee55b646983927b2dbdc8cdd66
gudnuf <gudnuf21@proton.me>
2025-12-05 12:45:19 -0800

    add lightning sends for spark

--- app/features/send/cashu-send-quote-hooks.ts
+++ app/features/send/cashu-send-quote-hooks.ts
@@ -161,8 +161,12 @@ export function useCreateCashuSendQuote() {
 }
 
 export function useInitiateCashuSendQuote({
+  onSuccess,
   onError,
-}: { onError: (error: Error) => void }) {
+}: {
+  onSuccess: (data: CashuSendQuote) => void;
+  onError: (error: Error) => void;
+}) {
   const userId = useUser((user) => user.id);
   const cashuSendQuoteService = useCashuSendQuoteService();
   const cashuSendQuoteCache = useCashuSendQuoteCache();
@@ -192,6 +196,7 @@ export function useInitiateCashuSendQuote({
     },
     onSuccess: (data) => {
       cashuSendQuoteCache.add(data);
+      onSuccess(data);
     },
     onError: onError,
     retry: (failureCount, error) => {
