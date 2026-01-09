70fb288df937d7fcc1c46f3f8f1c46a71ce750e4
gudnuf <gudnuf21@proton.me>
2026-01-08 19:19:38 -0800

    add useAccountOrDefault and remove search param loaders

--- app/routes/_protected.send.tsx
+++ app/routes/_protected.send.tsx
@@ -1,25 +1,11 @@
-import { Outlet } from 'react-router';
-import {
-  useAccounts,
-  useDefaultAccount,
-} from '~/features/accounts/account-hooks';
+import { Outlet, useSearchParams } from 'react-router';
+import { useAccountOrDefault } from '~/features/accounts/account-hooks';
 import { SendProvider } from '~/features/send';
-import type { Route } from './+types/_protected.send';
 
-export function clientLoader({ request }: Route.ClientLoaderArgs) {
-  const url = new URL(request.url);
-  const accountId = url.searchParams.get('accountId');
-  return { accountId };
-}
-
-export default function SendLayout({ loaderData }: Route.ComponentProps) {
-  const { accountId } = loaderData;
-  const { data: accounts } = useAccounts();
-  const defaultAccount = useDefaultAccount();
-
-  const initialAccount = accountId
-    ? (accounts.find((a) => a.id === accountId) ?? defaultAccount)
-    : defaultAccount;
+export default function SendLayout() {
+  const [searchParams] = useSearchParams();
+  const accountId = searchParams.get('accountId');
+  const initialAccount = useAccountOrDefault(accountId);
 
   return (
     <SendProvider initialAccount={initialAccount}>
