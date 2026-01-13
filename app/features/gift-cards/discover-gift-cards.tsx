acebd8a48e5fc63ceaeadc927721b2f9963283e8
gudnuf <gudnuf21@proton.me>
2026-01-13 13:31:21 -0800

    make discover section overflow to edge of screen

--- app/features/gift-cards/discover-gift-cards.tsx
+++ app/features/gift-cards/discover-gift-cards.tsx
@@ -17,27 +17,35 @@ export function DiscoverGiftCards({ giftCards }: DiscoverSectionProps) {
   return (
     <div className="view-transition-available w-full shrink-0">
       <h2 className="mb-3 px-4 text-white">Discover</h2>
-      <div
-        className={cn(
-          'overflow-x-auto pb-1',
-          isMobile
-            ? 'scrollbar-none'
-            : '[&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-white/20 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:h-1.5',
-        )}
-      >
-        <div className="flex w-max gap-3 px-4 pb-2">
-          {giftCards.map((card) => (
-            <LinkWithViewTransition
-              key={`${card.url}:${card.currency}`}
-              to={`/gift-cards/add/${encodeURIComponent(card.url)}/${card.currency}`}
-              transition="slideUp"
-              applyTo="newView"
-            >
-              <WalletCard size="sm" className="shrink-0">
-                <WalletCardBackground src={card.image} alt={card.name} />
-              </WalletCard>
-            </LinkWithViewTransition>
-          ))}
+      <div className="sm:px-4">
+        <div
+          className={cn(
+            'overflow-x-auto pb-1',
+            isMobile
+              ? 'scrollbar-none'
+              : '[&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-white/20 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar]:h-1.5',
+          )}
+        >
+          <div className="flex w-max gap-3 pb-2">
+            {giftCards.map((card, index) => (
+              <LinkWithViewTransition
+                key={`${card.url}:${card.currency}`}
+                to={`/gift-cards/add/${encodeURIComponent(card.url)}/${card.currency}`}
+                transition="slideUp"
+                applyTo="newView"
+              >
+                <WalletCard
+                  size="sm"
+                  className={cn(
+                    index === 0 && 'ml-4 sm:ml-0',
+                    index === giftCards.length - 1 && 'mr-4 sm:mr-0',
+                  )}
+                >
+                  <WalletCardBackground src={card.image} alt={card.name} />
+                </WalletCard>
+              </LinkWithViewTransition>
+            ))}
+          </div>
         </div>
       </div>
     </div>
