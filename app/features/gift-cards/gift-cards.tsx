9aa63cbd9e14e7995cb4311cda7f295493d65630
orveth <orveth@proton.me>
2026-05-12 14:52:16 -0700

    Solid page header on /gift-cards

--- app/features/gift-cards/gift-cards.tsx
+++ app/features/gift-cards/gift-cards.tsx
@@ -46,12 +46,12 @@ export function GiftCards() {
 
   return (
     <Page className="px-0 pb-0">
-      <PageHeader className="absolute inset-x-0 top-0 z-20 mb-0 px-4 pt-4 pb-4">
+      <PageHeader className="px-4">
         <ClosePageButton to="/" transition="slideLeft" applyTo="oldView" />
         <PageHeaderTitle>Gift Cards</PageHeaderTitle>
       </PageHeader>
 
-      <PageContent className="scrollbar-none relative min-h-0 overflow-y-auto px-0 pt-16 pb-0">
+      <PageContent className="scrollbar-none relative min-h-0 overflow-y-auto px-0 pb-0">
         <div className="flex w-full flex-col items-center gap-4">
           {giftCardsToDiscover.length > 0 && (
             <DiscoverGiftCards giftCards={giftCardsToDiscover} />
